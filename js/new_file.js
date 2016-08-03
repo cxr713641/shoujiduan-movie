f212dc81fa771fe7cbbb4c879ab5879fe48a216d
1 <2>
2016-08-03 18:59:29 +0800

    -

--- js/new_file.js
+++
@@ -1,9 +0,0 @@
-function fontAuto(width){
-	var srceenWidth=document.documentElement.clientWidth;
-	if(screenWith>=width){
-		document.documentElement.style.fonSize="625%";
-	}
-	else{
-		document.documentElement.Style.fontSize=(625*screenWidth/width)+"%"
-	}
-}
